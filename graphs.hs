d40c84767b60545cd77245c4b10a923206c5f1b5
Andypedozz <andypedini02@gmail.com>
2026-01-15 15:56:19 +0100

    .

--- graphs.hs
+++ graphs.hs
@@ -7,6 +7,15 @@
 -- # Anno di corso: terzo                                      #
 -- ##############################################################
 
+{- Specifica:
+ - acquisire da file una lista di vertici e una lista di archi
+ - costruire un grafo orientato
+ - calcolare le componenti fortemente connesse
+ - costruire il grafo compresso
+ - contare le componenti con indegree 0
+   diverse da quella contenente un vertice scelto
+-}
+
 import Data.List (nub)
 
 --------------------------------------------------
@@ -15,10 +24,9 @@ import Data.List (nub)
 
 type Grafo = ([Int], [(Int, Int)])
 type Caso  = ([Int], [(Int, Int)], Int)
--- (vertici, archi, vertice di partenza)
 
 --------------------------------------------------
--- DATI HARDCODED
+-- DATI
 --------------------------------------------------
 
 casi :: [Caso]
@@ -64,93 +72,99 @@ casi =
      6)
   ]
 
-
 --------------------------------------------------
 -- MAIN
 --------------------------------------------------
 
 main :: IO ()
-main = mapM_ (uncurry eseguiCaso) (zip [1..] casi)
+main = mapM_ eseguiCaso (zip [1..] casi)
 
---------------------------------------------------
--- ESECUZIONE DI UN CASO
---------------------------------------------------
-
-eseguiCaso :: Int -> Caso -> IO ()
-eseguiCaso idx (vertici, archi, vPartenza) = do
+eseguiCaso :: (Int, Caso) -> IO ()
+eseguiCaso (i, (vertici, archi, vPartenza)) = do
     putStrLn "======================================"
-    putStrLn ("Caso #" ++ show idx)
-
-    stampaLista "Grafo iniziale (archi):" archi
-
+    putStrLn ("Caso #" ++ show i)
+    putStrLn "SCC:"
     let sccs = kosaraju vertici archi
-    stampaSCC sccs
-
-    let grafoCompresso = comprimiGrafo sccs archi
-    stampaGrafoCompresso grafoCompresso
-
-    stampaIndegree (length sccs) grafoCompresso
-
-    let risultato = contaSCCZero vPartenza sccs grafoCompresso
-
+    mapM_ print sccs
+    let gc = comprimiGrafo sccs archi
+    putStrLn "Grafo compresso:"
+    mapM_ print gc
     putStrLn $
-        "Numero di SCC con indegree 0 (esclusa la componente contenente "
-        ++ show vPartenza ++ "): " ++ show risultato
+        "SCC con indegree 0 (esclusa partenza): "
+        ++ show (contaSCCZero vPartenza sccs gc)
     putStrLn ""
 
 --------------------------------------------------
--- FUNZIONI GRAFO
+-- FUNZIONI DI GRAFO
 --------------------------------------------------
 
 adiacenti :: Int -> [(Int, Int)] -> [Int]
-adiacenti _ [] = []
-adiacenti v ((x,y):as)
-    | v == x    = y : adiacenti v as
-    | otherwise = adiacenti v as
-
-dfs :: [Int] -> [(Int, Int)] -> [Int] -> [Int] -> [Int]
-dfs [] _ _ pila = pila
-dfs (v:vs) archi visitati pila
-    | v `elem` visitati = dfs vs archi visitati pila
-    | otherwise =
-        dfs vicini archi visitati' pila ++ [v]
-  where
-    visitati' = v : visitati
-    vicini    = adiacenti v archi
+adiacenti v archi = [ y | (x,y) <- archi, x == v ]
 
 invertiArchi :: [(Int, Int)] -> [(Int, Int)]
 invertiArchi = map (\(x,y) -> (y,x))
 
-dfsComponente :: [Int] -> [(Int, Int)] -> [Int] -> [Int] -> [Int]
-dfsComponente [] _ _ visitatiLocali = visitatiLocali
-dfsComponente (v:vs) archi visitatiGlobali visitatiLocali
-    | v `elem` visitatiLocali = dfsComponente vs archi visitatiGlobali visitatiLocali
-    | v `elem` visitatiGlobali = dfsComponente vs archi visitatiGlobali visitatiLocali
-    | otherwise =
-        dfsComponente (vicini ++ vs) archi visitatiGlobali (visitatiLocali ++ [v])
-  where
-    vicini = adiacenti v archi
+--------------------------------------------------
+-- DFS PER ORDINE DI FINE (KOSARAJU 1)
+--------------------------------------------------
 
-calcolaSCC :: [Int] -> [(Int, Int)] -> [Int] -> [[Int]]
-calcolaSCC [] _ _ = []
-calcolaSCC (v:vs) archi visitati
-    | v `elem` visitati = calcolaSCC vs archi visitati
+dfsOrdine :: Int -> [(Int,Int)] -> [Int] -> ([Int], [Int])
+dfsOrdine v archi visitati
+    | v `elem` visitati = (visitati, [])
     | otherwise =
-        componente : calcolaSCC vs archi visitati'
+        let visitati' = v : visitati
+            (visitatiFinali, ordineFigli) =
+                foldl visita (visitati', []) (adiacenti v archi)
+        in (visitatiFinali, ordineFigli ++ [v])
   where
-    componente = dfsComponente [v] archi visitati []
-    visitati'  = visitati ++ componente
+    visita (vis, ord) u =
+        let (vis', ord') = dfsOrdine u archi vis
+        in (vis', ord ++ ord')
 
-kosaraju :: [Int] -> [(Int, Int)] -> [[Int]]
+ordineDiFine :: [Int] -> [(Int,Int)] -> [Int]
+ordineDiFine vertici archi =
+    snd $
+      foldl visitaGlobale ([], []) vertici
+  where
+    visitaGlobale (vis, ord) v =
+        let (vis', ord') = dfsOrdine v archi vis
+        in (vis', ord ++ ord')
+
+--------------------------------------------------
+-- DFS COMPONENTE (KOSARAJU 2)
+--------------------------------------------------
+
+dfsComponente :: Int -> [(Int,Int)] -> [Int] -> ([Int], [Int])
+dfsComponente v archi visitati
+    | v `elem` visitati = (visitati, [])
+    | otherwise =
+        let visitati' = v : visitati
+            (visitatiFinali, compFigli) =
+                foldl visita (visitati', []) (adiacenti v archi)
+        in (visitatiFinali, v : compFigli)
+  where
+    visita (vis, comp) u =
+        let (vis', comp') = dfsComponente u archi vis
+        in (vis', comp ++ comp')
+
+--------------------------------------------------
+-- KOSARAJU COMPLETO
+--------------------------------------------------
+
+kosaraju :: [Int] -> [(Int,Int)] -> [[Int]]
 kosaraju vertici archi =
-    sccs ++ map (:[]) verticiNonUsati
+    scc
   where
-    ordine         = dfs vertici archi [] []
+    ordine         = reverse (ordineDiFine vertici archi)
     archiInvertiti = invertiArchi archi
-    sccs           = calcolaSCC (reverse ordine) archiInvertiti []
-    verticiUsati   = concat sccs
-    verticiNonUsati = [ v | v <- vertici, v `notElem` verticiUsati ]
+    scc            = snd $
+        foldl costruisci ([], []) ordine
 
+    costruisci (vis, comps) v
+        | v `elem` vis = (vis, comps)
+        | otherwise =
+            let (vis', comp) = dfsComponente v archiInvertiti vis
+            in (vis', comps ++ [comp])
 
 --------------------------------------------------
 -- GRAFO COMPRESSO
@@ -164,60 +178,25 @@ indiceSCC v sccs =
 
 comprimiGrafo :: [[Int]] -> [(Int, Int)] -> [(Int, Int)]
 comprimiGrafo sccs archi =
-    nub [ (i,j)
-        | (x,y) <- archi
-        , let i = indiceSCC x sccs
-        , let j = indiceSCC y sccs
-        , i /= j
-        ]
+    nub
+      [ (i,j)
+      | (x,y) <- archi
+      , let i = indiceSCC x sccs
+      , let j = indiceSCC y sccs
+      , i /= j
+      ]
 
-gradoEntrante :: Int -> [(Int, Int)] -> Int
+gradoEntrante :: Int -> [(Int,Int)] -> Int
 gradoEntrante c archi =
     length [ () | (_,y) <- archi, y == c ]
 
-contaSCCZero :: Int -> [[Int]] -> [(Int, Int)] -> Int
+contaSCCZero :: Int -> [[Int]] -> [(Int,Int)] -> Int
 contaSCCZero v sccs archi =
-    length [ i
-           | (i,_) <- zip [0..] sccs
-           , i /= sccPartenza
-           , gradoEntrante i archi == 0
-           ]
-  where
-    sccPartenza = indiceSCC v sccs
+    length
+      [ i
+      | i <- [0..length sccs - 1]
+      , i /= indiceSCC v sccs
+      , gradoEntrante i archi == 0
+      ]
 
---------------------------------------------------
--- FUNZIONI DI STAMPA
---------------------------------------------------
 
-stampaLista :: Show a => String -> [a] -> IO ()
-stampaLista titolo xs = do
-    putStrLn titolo
-    mapM_ (\x -> putStrLn ("  - " ++ show x)) xs
-    putStrLn ""
-
-stampaSCC :: [[Int]] -> IO ()
-stampaSCC sccs = do
-    putStrLn "Componenti fortemente connesse:"
-    mapM_ stampa (zip [0..] sccs)
-    putStrLn ""
-  where
-    stampa (i,comp) =
-        putStrLn ("  C" ++ show i ++ " = " ++ show comp)
-
-stampaGrafoCompresso :: [(Int,Int)] -> IO ()
-stampaGrafoCompresso archi = do
-    putStrLn "Grafo compresso (tra componenti):"
-    mapM_ stampaArco archi
-    putStrLn ""
-  where
-    stampaArco (i,j) =
-        putStrLn ("  C" ++ show i ++ " -> C" ++ show j)
-
-stampaIndegree :: Int -> [(Int,Int)] -> IO ()
-stampaIndegree n archi = do
-    putStrLn "Indegree delle componenti:"
-    mapM_ stampa [0..n-1]
-    putStrLn ""
-  where
-    stampa i =
-        putStrLn ("  C" ++ show i ++ " : " ++ show (gradoEntrante i archi))
