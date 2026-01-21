46583a850c03859b9fd84244c8080ee6c9d47f20
Andypedozz <andypedini02@gmail.com>
2026-01-21 02:48:39 +0100

    .

--- graphFile.pl
+++ graphFile.pl
@@ -1,24 +1,47 @@
 % ##############################################################
 % # Corso di Programmazione Logica e Funzionale                #
 % # Progetto per la sessione autunnale A.A. 2024/2025          #
-% # Versione Prolog (GProlog)                                  #
+% # Versione GNU Prolog                                        #
 % # di Andrea Pedini                                           #
 % ##############################################################
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% LETTURA GRAFO DA FILE
+%% LETTURA GRAFO DA FILE (compatibile GNU Prolog)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% legge una riga dal file e la converte in atom
+leggi_linea(Stream, Line) :-
+    get_char(Stream, Char),
+    leggi_linea_aux(Stream, Char, Chars),
+    atom_chars(Line, Chars).
+
+leggi_linea_aux(_, end_of_file, []) :- !.
+leggi_linea_aux(_, '\n', []) :- !.
+leggi_linea_aux(Stream, Char, [Char|Chars]) :-
+    get_char(Stream, NextChar),
+    leggi_linea_aux(Stream, NextChar, Chars).
+
+% legge un termine da una riga del file
 leggi_termine(Stream, Termine) :-
-    read_line(Stream, Line),
+    leggi_linea(Stream, Line),
     atom_concat(Line, '.', LineConPunto),
     read_from_atom(LineConPunto, Termine).
 
-leggi_grafo_da_file(File, grafo(Nodi,Archi)) :-
+% legge l'intero grafo da file
+leggi_grafo_da_file(File, grafo(Nodi, Archi)) :-
     open(File, read, Stream),
     leggi_termine(Stream, Nodi),
     leggi_termine(Stream, Archi),
     close(Stream).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% CONVERSIONE NUMERO DA INPUT (senza punto finale)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+leggi_numero(N) :-
+    leggi_linea(user_input, Line),
+    atom_codes(Line, Codes),
+    number_codes(N, Codes).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% PREDICATI DI BASE
@@ -30,18 +53,15 @@ membro(X,[_|T]) :- membro(X,T).
 nodi(grafo(N,_), N).
 archi(grafo(_,A), A).
 
-adiacente(grafo(_,A), X, Y) :-
-    membro((X,Y), A).
+adiacente(grafo(_,A), X, Y) :- membro((X,Y), A).
 
-adiacenti(G, X, L) :-
-    findall(Y, adiacente(G,X,Y), L).
+adiacenti(G, X, L) :- findall(Y, adiacente(G,X,Y), L).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% DFS CON ORDINE DI COMPLETAMENTO
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dfs_visit(_, N, V, V, []) :-
-    membro(N,V), !.
+dfs_visit(_, N, V, V, []) :- membro(N,V), !.
 
 dfs_visit(G, N, V, V2, Ordine) :-
     \+ membro(N,V),
@@ -51,6 +71,7 @@ dfs_visit(G, N, V, V2, Ordine) :-
     V2 = V1.
 
 dfs_lista(_, [], V, V, []).
+
 dfs_lista(G, [H|T], V, V2, Ordine) :-
     dfs_visit(G, H, V, V1, O1),
     dfs_lista(G, T, V1, V2, O2),
@@ -65,9 +86,10 @@ dfs_grafo(G, Ordine) :-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 trasposto(grafo(N,A), grafo(N,AT)) :-
-    trasponi_archi(A,AT).
+    trasponi_archi(A, AT).
 
 trasponi_archi([], []).
+
 trasponi_archi([(X,Y)|T], [(Y,X)|R]) :-
     trasponi_archi(T,R).
 
@@ -75,8 +97,7 @@ trasponi_archi([(X,Y)|T], [(Y,X)|R]) :-
 %% DFS PER UNA SCC
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dfs_scc(_, N, V, V, []) :-
-    membro(N,V), !.
+dfs_scc(_, N, V, V, []) :- membro(N,V), !.
 
 dfs_scc(G, N, V, V2, [N|Comp]) :-
     \+ membro(N,V),
@@ -84,6 +105,7 @@ dfs_scc(G, N, V, V2, [N|Comp]) :-
     dfs_scc_lista(G, Vicini, [N|V], V2, Comp).
 
 dfs_scc_lista(_, [], V, V, []).
+
 dfs_scc_lista(G, [H|T], V, V2, Comp) :-
     dfs_scc(G, H, V, V1, C1),
     dfs_scc_lista(G, T, V1, V2, C2),
@@ -100,6 +122,7 @@ kosaraju(G, SCCs) :-
     kosaraju_visita(GT, OrdInv, [], SCCs).
 
 kosaraju_visita(_, [], _, []).
+
 kosaraju_visita(G, [N|T], V, SCCs) :-
     membro(N,V), !,
     kosaraju_visita(G, T, V, SCCs).
@@ -112,10 +135,8 @@ kosaraju_visita(G, [N|T], V, [SCC|R]) :-
 %% GRAFO DELLE SCC
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-scc_di_nodo(N, [S|_], S) :-
-    membro(N,S), !.
-scc_di_nodo(N, [_|T], S) :-
-    scc_di_nodo(N,T,S).
+scc_di_nodo(N, [S|_], S) :- membro(N,S), !.
+scc_di_nodo(N, [_|T], S) :- scc_di_nodo(N,T,S).
 
 arco_scc(G, SCCs, S1, S2) :-
     archi(G,A),
@@ -151,7 +172,7 @@ main :-
     stampa_scc(SCCs),
 
     write('Inserisci nodo di partenza: '),
-    read(Nodo),
+    leggi_numero(Nodo),
     scc_di_nodo(Nodo, SCCs, SCCpartenza),
 
     findall(
